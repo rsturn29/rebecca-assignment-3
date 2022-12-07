5850c2e7d7978548fdcff130d42eda0058d00fd8
Becks <101711019+rsturn29@users.noreply.github.com>
2022-12-07 16:06:40 -0600

    format changes

--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,12 +1,12 @@
 package com.coderscampus;
 
 public class UserService {
-public User validateUser(String username, String password) {
-	for(User user : UserLoginApplication.users) {
-		if(user.getUsername().equalsIgnoreCase(username)&& user.getPassword().equals(password)) {
-			return user;
+	public User validateUser(String username, String password) {
+		for (User user : UserLoginApplication.users) {
+			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+				return user;
+			}
 		}
+		return null;
 	}
-	return null;
-}
 }
